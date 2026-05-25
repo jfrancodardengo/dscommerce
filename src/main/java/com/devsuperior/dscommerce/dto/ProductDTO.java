1c10b273478a71e79a2e91b1506836d203cb511b
jfrancodardengo <jfrancodardengo@gmail.com>
2026-05-25 10:15:05 -0300

    Exception handling

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -9,15 +9,16 @@ public class ProductDTO {
   private Double price;
   private String imgUrl;
 
-  public ProductDTO() {    
-  }
-
   public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
     this.imgUrl = imgUrl;
+  }
+
+  public ProductDTO() {
+    
   }
 
   public ProductDTO(Product entity) {
